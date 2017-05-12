0983c21f9ac71d809e47aaab7ffad7fb6a7cab1a
Sharabesh Ramesh <sharabesh@berkeley.edu>
2017-05-11 22:01:26 -0700

    Added redirect message to heroku

--- main.py
+++ main.py
@@ -1,36 +1,18 @@
-import urllib
-
-import tornado.httpserver
-import tornado.ioloop
-import tornado.web
-import os
-import tornado
-
-from urllib.parse import urlparse
+import flask Winter Safety Tips
+from flask import render_template
+from flask import Flask
 
 
+app = Flask(__name__)
 
-class MainHandler(tornado.web.RequestHandler):
-    def get(self):
-        self.render("index.html")
+@app.route("/")
+def get():
+    return render_template("index.html")
 
-
-class EmailHandler(tornado.web.RequestHandler):
-    def post(self):
-        username = self.get_body_argument("Full Name")
-        self.write(username)
-
-
-
-
-def make_app():
-    return tornado.web.Application([
-        (r"/", MainHandler),
-    ])
 
 
 
 if __name__ == "__main__":
-    app = make_app()
-    app.listen(5000)
-    tornado.ioloop.IOLoop.current().start()
+    app.debug = True
+    app.run()
+
